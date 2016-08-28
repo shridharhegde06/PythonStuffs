f74289a01fec03ed130b4e63efa74a08822254f1
Anil Admin <msanilgoudar@gmail.com>
2016-08-28 11:24:39 +0530

    Added simple GUI kit

--- breakTime.py
+++ breakTime.py
@@ -1,17 +1,25 @@
-#This python module opens a youtube video at intervals of time
-#opening browser window
+#This python module opens triggers a youtube video afte continue 2 hours of work.
 import webbrowser
 import time
-#import Tkinter
-#import tkMessageBox
+from tkinter import *
 
-#import ctypes
-#making program to wait for given period amount of time
-#print("Starting at "+ time.ctime())
-for i in range(3):
- #   ctypes.windll.user32.MessageBoxA(0, "Your text", "Your title", 1)
-   # top=Tkinter.Tk()
-    #top.withdraw()
-    #tkMessageBox.showinfo("Break","chill ")
-    time.sleep(10) #argument will is in seconds
+def playVideo():
     webbrowser.open("https://www.youtube.com/watch?v=k4yXQkG2s1E")
+
+def doNothing():
+    global alertWin
+    alertWin.destroy()
+#How many times should program work
+for i in range(5):
+    time.sleep(1*60*60) #argument will is in seconds
+    #creating alert window
+    alertWin=Tk()
+    #setting up title
+    alertWin.title("Break Time Window !!")
+    label1=Label(alertWin,text="Taka a break !!",fg="red").grid(row=0,column=3)
+    #creating ok button and play video if ok
+    ok=Button(alertWin,text="Ok",command=playVideo).grid(row=5,column=1)
+    #creating cancel button and play nothing to continue to work
+    notOk = Button(alertWin,text="Not Ok",command=doNothing).grid(row=5,column=5)
+    alertWin.mainloop()
+   
